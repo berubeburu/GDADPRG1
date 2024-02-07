18ecb60019a46d7e27ae12dae0e6d787f21eacb0
berubeburu <156291523+berubeburu@users.noreply.github.com>
2024-02-07 21:08:12 +0800

    Lesson 5

--- gdgrap1.cpp
+++ gdgrap1.cpp
@@ -8,13 +8,25 @@
 
 #define TINYOBJLOADER_IMPLEMENTATION
 #include "tiny_obj_loader.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 // modifier for the model's x position
-float x_mod = -2.f;;
+float x_mod = 0;
 // modifier for the model's y position
-float y_mod = -2.f;;
+float y_mod = 0;
 // modifier for the model's z position
 float z_mod = -2.f;
+// modifier for the model's theta
+float theta_mod = 0;
+// modifier for the model's x axis
+float axis_x_mod = 0;
+// modifier for the model's y axis
+float axis_y_mod = 0;
+// modifier for the model's x scale
+float scale_x_mod = 1;
+// modifier for the model's y scale
+float scale_y_mod = 1;
 
 void Key_CallBack(GLFWwindow* window, // pointer to the window
     int key, // keycode of the press
@@ -23,28 +35,74 @@ void Key_CallBack(GLFWwindow* window, // pointer to the window
     int mods) // which modifier keys is held down
 {
     // when user presses D
-    if (key == GLFW_KEY_D &&
-        action == GLFW_PRESS) {
+    if (key == GLFW_KEY_D) {
         // move bunny to right
         x_mod += 0.1f;
     }
     // when user presses A
-    if (key == GLFW_KEY_A &&
-        action == GLFW_PRESS) {
+    if (key == GLFW_KEY_A) {
         // move bunny to left
         x_mod -= 0.1f;
     }
     // when user presses S
-    if (key == GLFW_KEY_S &&
-        action == GLFW_PRESS) {
+    if (key == GLFW_KEY_S) {
         // move bunny down
-        z_mod += -0.3f;
+        y_mod -= 0.1f;
     }
     // when user presses W
-    if (key == GLFW_KEY_W &&
-        action == GLFW_PRESS) {
+    if (key == GLFW_KEY_W) {
         // move bunny up
-        z_mod -= -0.3f;
+        y_mod += 0.1f;
+    }
+    // when user presses up
+    if (key == GLFW_KEY_UP) {
+        // rotate bunny up
+        theta_mod -= 10;
+        axis_x_mod = 1;
+        axis_y_mod = 0;
+    }
+    // when user presses down
+    if (key == GLFW_KEY_DOWN) {
+        // rotate bunny down
+        theta_mod += 10;
+        axis_x_mod = 1;
+        axis_y_mod = 0;
+    }
+    // when user presses left
+    if (key == GLFW_KEY_LEFT) {
+        // rotate bunny left
+        theta_mod -= 10;
+        axis_y_mod = 1;
+        axis_x_mod = 0;
+    }
+    // when user presses right
+    if (key == GLFW_KEY_RIGHT) {
+        // rotate bunny right
+        theta_mod += 10;
+        axis_y_mod = 1;
+        axis_x_mod = 0;
+    }
+    // when user presses Q
+    if (key == GLFW_KEY_Q) {
+        // decrease bunny
+        scale_x_mod -= 0.1f;
+        scale_y_mod -= 0.1f;
+    }
+    // when user presses E
+    if (key == GLFW_KEY_E) {
+        // increase bunny
+        scale_x_mod += 0.1f;
+        scale_y_mod += 0.1f;
+    }
+    // when user presses Z
+    if (key == GLFW_KEY_Z) {
+        // zoom in
+        z_mod -= -0.1f;
+    }
+    // when user presses X
+    if (key == GLFW_KEY_X) {
+        // zoom out
+        z_mod += -0.1f;
     }
 }
 
@@ -54,7 +112,7 @@ int main(void)
 {
     glm::mat4 identity_matrix = glm::mat4(1.0f);
 
-    float x = 0, y = 0, z = 0, scale_x = 1.5, scale_y = 1.5, scale_z = 1.5, theta = 0, axis_x = 0, axis_y = 0, axis_z = 1;
+    float x = 0, y = 0, z = 0, scale_x = 1, scale_y = 1, scale_z = 1, theta = 0, axis_x = 0, axis_y = 0, axis_z = 1;
 
     GLFWwindow* window;
 
@@ -79,8 +137,51 @@ int main(void)
 
     glViewport(0, 0, window_width, window_height);
 
+    int img_width, // width of texture
+        img_height, // height of texture
+        colorChannels; // number of color channels
+
+    // fix the flipped texture
+    stbi_set_flip_vertically_on_load(true);
+
+    // load the texture and fill out the variables
+    unsigned char* tex_bytes =
+        stbi_load("3D/ayaya.png", // texture path
+            &img_width, // fills out the width
+            &img_height, // fills out the height
+            &colorChannels,// fills out the color channel
+            0);
+
+    // OpenGL reference to the texture
+    GLuint texture;
+    // generate a reference
+    glGenTextures(1, &texture);
+    // set the current texture we're working with on to Texture 0
+    glActiveTexture(GL_TEXTURE0);
+    // bind out next tasks to tex0 to our current refernce similar to what wer're doing to VBOs
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    // assign the loaded texture to the OpenGL reference
+    glTexImage2D(GL_TEXTURE_2D,
+        0, // texture 0
+        GL_RGBA, // target color format of the texture
+        img_width, // texture width
+        img_height, // texture height
+        0,
+        GL_RGBA, // color format of the texture
+        GL_UNSIGNED_BYTE,
+        tex_bytes); // loaded texture in bytes
+
+    // generate the mipmaps to the current texture
+    glGenerateMipmap(GL_TEXTURE_2D);
+    // free up the loaded bytes
+    stbi_image_free(tex_bytes);
+
+    // enable depth testing
+    glEnable(GL_DEPTH_TEST);
+
     // set the callback function to the window
-    //glfwSetKeyCallback(window, Key_CallBack);
+    glfwSetKeyCallback(window, Key_CallBack);
 
     // load the shader file into a string stream        vertex
     fstream vertSrc("Shaders/sample.vert");
@@ -141,7 +242,7 @@ int main(void)
     // finalize the compilation process
     glLinkProgram(shaderProg);
 
-    string path = "3D/bunny.obj";
+    string path = "3D/myCube.obj";
     vector<tinyobj::shape_t> shapes;
     vector<tinyobj::material_t> material;
     string warning, error;
@@ -156,6 +257,17 @@ int main(void)
         &error,
         path.c_str()
     );
+
+    GLfloat UV[]{
+        0.f, 1.f,
+        0.f, 0.f,
+        1.f, 1.f,
+        1.f, 0.f,
+        1.f, 1.f,
+        1.f, 0.f,
+        0.f, 1.f,
+        0.f, 0.f
+    };
 
     // get the EBO indices array 
     vector<GLuint> mesh_indices;
@@ -176,10 +288,11 @@ int main(void)
         0,1,2
     };
 
-    GLuint VAO, VBO, EBO;
+    GLuint VAO, VBO, EBO, VBO_UV;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
+    glGenBuffers(1, &VBO_UV);
 
     // currently editing VAO = null
     glBindVertexArray(VAO);
@@ -210,6 +323,26 @@ int main(void)
 
     glEnableVertexAttribArray(0);
 
+    // bind the UV buffer
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_UV);
+
+    glBufferData(GL_ARRAY_BUFFER,
+        sizeof(GLfloat) * (sizeof(UV) / sizeof(UV[0])), // float * size of the UV array
+        &UV[0], // the UV array earlier
+        GL_DYNAMIC_DRAW);
+
+    glVertexAttribPointer(
+        2, // 2 for the UV or tex coords
+        2, // UV
+        GL_FLOAT, // type of array
+        GL_FALSE,
+        2 * sizeof(float), // every 2 index
+        (void*)0
+    );
+
+    // enable 2 
+    glEnableVertexAttribArray(2);
+
     // currently editing VBO = VBO
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     // currently editing VBO = null
@@ -234,14 +367,61 @@ int main(void)
         100.f // zfar
     );
 
+    // position of the camera in the world / eye
+    glm::vec3 cameraPos = glm::vec3(x_mod, 0, 10.f);
+
+    // construct the position matrix using the eye
+    glm::mat4 cameraPositionMatrix =
+        glm::translate(glm::mat4(1.0f), // initialize as identity matrix
+            cameraPos * -1.0f); // multiply to -1 since we need -P
+
+    // world's up direction; normally just 1 in Y
+    glm::vec3 WorldUp = glm::vec3(0, 1.0f, 0);
+    // camera's center
+    glm::vec3 Center = glm::vec3(0, 3.f, 0);
+    // get the forward
+    glm::vec3 F = glm::vec3(Center - cameraPos);
+    // normalize the forward
+    F = glm::normalize(F);
+    // get the right
+    glm::vec3 R = glm::normalize(glm::cross(F, WorldUp));
+    // get the up
+    glm::vec3 U = glm::normalize(glm::cross(R, F));
+
+    // construct the orientation matrix
+    glm::mat4 cameraOrientation = glm::mat4(1.f);
+
+    // manually assign the matrix[column][row]
+    cameraOrientation[0][0] = R.x;
+    cameraOrientation[1][0] = R.y;
+    cameraOrientation[2][0] = R.z;
+
+    cameraOrientation[0][1] = U.x;
+    cameraOrientation[1][1] = U.y;
+    cameraOrientation[2][1] = U.z;
+
+    cameraOrientation[0][2] = -F.x;
+    cameraOrientation[1][2] = -F.y;
+    cameraOrientation[2][2] = -F.z;
+
+    // camera view matrix
+    //glm::mat4 viewMatrix = cameraOrientation * cameraPositionMatrix;
+    glm::mat4 viewMatrix = glm::lookAt(cameraPos, Center, WorldUp);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        x = x_mod;
+        y = y_mod;
         z = z_mod;
-        theta += 0.04;
+        theta = theta_mod;
+        axis_x = axis_x_mod;
+        axis_y = axis_y_mod;
+        scale_x = scale_x_mod;
+        scale_y = scale_y_mod;
 
         // Start with the translation matrix
         glm::mat4 transformation_matrix = glm::translate(
@@ -273,9 +453,22 @@ int main(void)
             GL_FALSE, // transpose
             glm::value_ptr(transformation_matrix)); // pointer to the matrix
 
+        unsigned int viewLoc = glGetUniformLocation(shaderProg, "view");
+        glUniformMatrix4fv(viewLoc, // address of the variable
+            1, // how many values are we modifying
+            GL_FALSE,
+            glm::value_ptr(viewMatrix)); // view matrix
+
         // tell open GL to use this shader for the VAO/s below
         glUseProgram(shaderProg);
         glBindVertexArray(VAO);
+
+        // get the location of tex 0 in the fragment shader
+        GLuint tex0Address = glGetUniformLocation(shaderProg, "tex0");
+        // tell openGL to use the texture
+        glBindTexture(GL_TEXTURE_2D, texture);
+        // use the texture at 0
+        glUniform1i(tex0Address, 0);
 
         //glDrawArrays(GL_TRIANGLES, 0, 3);
         glDrawElements(GL_TRIANGLES, mesh_indices.size(), GL_UNSIGNED_INT, 0);
